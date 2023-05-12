d2ba7b20bd3cfd0d27ef8297bbb057903f6b6cae
filiphis <l.filiphis@hotmail.com>
2023-05-12 17:23:27 -0300

    Desenvolvendo a galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -4,7 +4,16 @@ import styles from "./styles.module.scss";
 export default function Galeria() {
   return (
     <div className={styles.galeria}>
-      <h1>Galeria</h1>
+      <h2 className={styles.galeria__title}>Navegue pela galeria</h2>
+      <div className={styles.galeria__tags__container}>
+        <span>Busque por tags:</span>
+        <ul className={styles.tags__list}>
+          <li className={styles.tags__item}>Es</li>
+          <li className={styles.tags__item}>Est</li>
+          <li className={styles.tags__item}>Est</li>
+          <li className={styles.tags__item}>Estre</li>
+        </ul>
+      </div>
     </div>
   );
 }
